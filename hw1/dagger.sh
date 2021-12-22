8e32aa8ec389955795c0fd37b058be9e5b25a447
VincentWang <wfuymu@gmail.com>
2021-12-21 22:03:24 -0500

    hw1 finish

---
+++ hw1/dagger.sh
@@ -0,0 +1,5 @@
+python cs285/scripts/run_hw1.py --expert_policy_file cs285/policies/experts/Ant.pkl --env_name Ant-v2 --exp_name dagger_ant --n_iter 10 --do_dagger --expert_data cs285/expert_data/expert_data_Ant-v2.pkl
+python cs285/scripts/run_hw1.py --expert_policy_file cs285/policies/experts/HalfCheetah.pkl --env_name HalfCheetah-v2 --exp_name dagger_hct --n_iter 10 --do_dagger --expert_data cs285/expert_data/expert_data_HalfCheetah-v2.pkl
+python cs285/scripts/run_hw1.py --expert_policy_file cs285/policies/experts/Hopper.pkl --env_name Hopper-v2 --exp_name dagger_hpr --n_iter 10 --do_dagger --expert_data cs285/expert_data/expert_data_Hopper-v2.pkl
+python cs285/scripts/run_hw1.py --expert_policy_file cs285/policies/experts/Humanoid.pkl --env_name Humanoid-v2 --exp_name dagger_hmd --n_iter 10 --do_dagger --expert_data cs285/expert_data/expert_data_Humanoid-v2.pkl
+python cs285/scripts/run_hw1.py --expert_policy_file cs285/policies/experts/Walker2d.pkl --env_name Walker2d-v2 --exp_name dagger_wkr --n_iter 10 --do_dagger --expert_data cs285/expert_data/expert_data_Walker2d-v2.pkl 
